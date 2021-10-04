c9312ba23b5932ed1bb8c5d0fa9f4b46286ccab8
juan.sanchez@mikroways.com <juanpsanchezm@gmail.com>
2021-10-04 19:35:29 -0300

    Comentario de por que no una clase abstracta para instrumentos

--- src/p5/Piano.java
+++ src/p5/Piano.java
@@ -1,6 +1,10 @@
 package p5;
 
 public class Piano implements InstrumentoMusical {
+	// Esta clase la hacemos rapidamente para probar,
+	// es muy parecida a guitarra, por lo que sería mas
+	// correcto hacer una superclase y que esta clase y
+	// Guitarra hereden de ella.
 	public void hacerSonar(){
 		System.out.println("Suena  " + queEs());
 	}
